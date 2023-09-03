935b6a7ab6234ee8e3403d77616e02e7bb1ebf5c
Santy Cierco <santidisturbios@gmail.com>
2023-09-03 10:41:26 +0200

    Update class.js

--- class.js
+++ class.js
@@ -28,6 +28,25 @@ class User extends Greeter {
   } 
 }
 
+
+class Book {
+  author = "Hari";
+  publisher = "Apress";
+  getPublisher = () => {
+    console.log(this.publisher);
+  };
+}
+class ReactBook extends Book {
+  name = "Just React";
+  publishedYear = 2022;
+}
+const reactBook = new ReactBook();
+console.log(reactBook.name);
+reactBook.getPublisher();
+
+
+
+
 const user = new User({ name: "Raphael", surname:"Perez" });
 
 console.log(user.greet());
